bc93945ea9a1b64ecc55a186c33761c672f96f8e
Erik7354 <Erik7354@gmail.com>
2020-01-30 23:28:42 +0100

    adding new Version plus test website

---
+++ Version 2.0/js/init.js
@@ -0,0 +1,43 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+
+var instance = M.Carousel.init({
+  fullWidth: true,
+  indicators: true
+});
+
+// Or with jQuery
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
