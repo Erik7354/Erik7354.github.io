d26301a73ec01eb27d75a9c4cc2008a40f4c001a
Erik7354 <31774332+Erik7354@users.noreply.github.com>
2020-05-03 18:36:23 +0200

    fixed gallery (hopefully for ever) and added icon (browser tab)

--- thb Webseite/js/gallery.js
+++ thb Webseite/js/gallery.js
@@ -1,5 +1,8 @@
-var title = "Constructing HTML Elements";
+/////////////////////////////
+// JS - Gallerie Generator //
+/////////////////////////////
 
+// Setting the Header for the gallery; headline img and <hr>
 var gallery = [
     '<div>',
         '<h2>Gallerie</h2>',
@@ -8,43 +11,41 @@ var gallery = [
                 '<img class="materialboxed" id="headlinerImg" style="width: 100%;" src="../img/gallery/headliner.jpg">',
             '</div>',
             '<div class="col s12">',
-                '<hr>',
+                '<hr id="headlineTrenner">',
             '</div>',
             '<div id="imgMatrix">\n</div>'
 ];
-// html: '<div ...>\n<h1 ...>Constructing HTML Elements<h1>\n</div>'
-
-/*var imgElement = [
-    '<div class="s6 m4>',
-        '<img src="">',
-    '</div>'
-]*/
-
+// Adding the Header to the website
 document.getElementById('imageContainer').innerHTML += gallery.join("\n");
-try{
-    for (let i = 1; i < 21; i++) {
 
-        var img = '<img src="../img/gallery/' + String(i) + '.jpg" class="matrixImg" alt="" id="matrixImg' + String(i) + '">';
+// for Schleife für Max 21 Bilder / 7 Reihen
+for (let i = 1; i < 21; i++) {
+    // Bild Tag mit Dateipfad wird zusammengesetzt
+    var img = '<img src="../img/gallery/' + String(i) + '.jpg" class="matrixImg" alt="" id="matrixImg' + String(i) + '">';
     
-        img = [
-            '<div class="col s6 m4">',
-            img,
-            '</div>'
-        ].join("\n");
+    // Bild wird in Column gepackt und zu einem String verarbeitet
+    img = [
+        '<div class="col s6 m4">',
+        img,
+        '</div>'
+    ].join("\n");
 
-        document.getElementById('imgMatrix').innerHTML += img;
-        
-        document.getElementById("matrixImg" + String(i)).onerror = function(){
-            //document.getElementById("matrixImg" + String(i)).parentElement.style.visibility = "hidden";
-            var element = document.getElementById("matrixImg" + String(i));
-            element.parentNode.removeChild(element);
-        }
-
+    // Bild wird hinzugefügt
+    document.getElementById('imgMatrix').innerHTML += img;
+    
+    // löscht das Bild auf der Webseite falls es nicht gefunden wurde; Error Message und Element bleibt
+    document.getElementById("matrixImg" + String(i)).onerror = function(){
+        //document.getElementById("matrixImg" + String(i)).parentElement.style.visibility = "hidden";
+        var element = document.getElementById("matrixImg" + String(i));
+        element.parentNode.removeChild(element);
     }
-}
-finally{
+
+    // added nach drei Bildern (einer Zeile) einen <hr> Tag
+    if (i%3 == 0) {
+    document.getElementById('imgMatrix').innerHTML += '<div class="col s12"><hr class="matrixImgTrenner"></div>';
+    }
 
 }
 
-
+// Schließende Div Tags für die Row und das overall Div
 document.getElementById('imageContainer').innerHTML += "\n</div>\n</div>";
